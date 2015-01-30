a8a9f0547708bc11a938579a8de9ac4f6bbe32ab
aday17@uw.edu <aday17@uw.edu@385cc09a-a65a-aa53-844a-8a29cb244b3c>
2015-01-30 08:35:51 +0000

    cause

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
 import java.awt.EventQueue;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.DropMode;
 
 
 public class Main {
 
-	private JFrame frame;
+	private JFrame mainFrame;
+	private JTextField userNameTF;
+	private JTextField pinTF;
 
 	/**
 	 * Launch the application.
@@ -15,7 +24,7 @@ public class Main {
 			public void run() {
 				try {
 					Main window = new Main();
-					window.frame.setVisible(true);
+					window.mainFrame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -34,9 +43,69 @@ public class Main {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame = new JFrame();
+		mainFrame.setBounds(100, 100, 450, 300);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.getContentPane().setLayout(null);
+		
+		JLabel lblLogin = new JLabel("Login:");
+		lblLogin.setBounds(176, 76, 46, 14);
+		mainFrame.getContentPane().add(lblLogin);
+		
+		userNameTF = new JTextField();
+		userNameTF.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent arg0) {
+				if(userNameTF.getText().isEmpty()) {
+					userNameTF.setText("UserName");
+				}
+			}
+		});
+		userNameTF.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(userNameTF.getText().equalsIgnoreCase("Username")) {
+					userNameTF.setText("");
+				}
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				if(userNameTF.getText().isEmpty()) {
+					userNameTF.setText("Username");
+				}
+			}
+		});
+		userNameTF.setText("Username");
+		userNameTF.setBounds(186, 101, 86, 20);
+		mainFrame.getContentPane().add(userNameTF);
+		userNameTF.setColumns(10);
+		
+		pinTF = new JTextField();
+		pinTF.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent arg0) {
+				if(pinTF.getText().isEmpty()) {
+					pinTF.setText("PIN");
+				}
+			}
+		});
+		pinTF.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(pinTF.getText().equalsIgnoreCase("PIN")) {
+					pinTF.setText("");
+				}
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				if(pinTF.getText().isEmpty()) {
+					pinTF.setText("PIN");
+				}
+			}
+		});
+		pinTF.setText("PIN");
+		pinTF.setBounds(186, 144, 86, 20);
+		mainFrame.getContentPane().add(pinTF);
+		pinTF.setColumns(10);
 	}
-
 }
