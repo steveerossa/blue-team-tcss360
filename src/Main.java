f9bd9c0ae6975f3e0e230ca2a20ed7f8fc18e036
aday17@uw.edu <aday17@uw.edu@385cc09a-a65a-aa53-844a-8a29cb244b3c>
2015-01-30 02:44:26 +0000

    application window start from window builder

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,42 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
 
 public class Main {
 
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//subclipse plugin second commit test.....123?
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Main() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
