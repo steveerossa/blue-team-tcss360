2cbff0fad9582ea8357929c42dac614f20576c2c
jstowe77@uw.edu <jstowe77@uw.edu@385cc09a-a65a-aa53-844a-8a29cb244b3c>
2015-03-08 20:00:13 +0000

    Database is working with Excel!!!! Fully populates the database from reading in the Excel file. Only clean data is left.

--- src/Model/Database.java
+++ src/Model/Database.java
@@ -23,6 +23,11 @@ public class Database {
 		populateData();
 	}
 
+	/**
+	 * Method to populate the database with the associated Excel spreadsheet. At current the spreadsheet
+	 * must be named "Answers.xlsx" in order to run properly. May need to include a prompt to user to select
+	 * data base file in next iteration.
+	 */
 	private void populateData() {
 		try
         {
@@ -41,24 +46,44 @@ public class Database {
                 Row row = rowIterator.next();
                 //For each row, iterate through all the columns
                 Iterator<Cell> cellIterator = row.cellIterator();
+                String [] rowContents = new String[5];
+            	int editLevel = 0;
+            	int i = 0;
                  
                 while (cellIterator.hasNext())
                 {
                     Cell cell = cellIterator.next();
+                    
                     //Check the cell type and format accordingly
                     switch (cell.getCellType())
                     {
                         case Cell.CELL_TYPE_NUMERIC:
-                            System.out.print(cell.getNumericCellValue() + "t");
+                        	editLevel = (int) cell.getNumericCellValue();
+                            //System.out.print(cell.getNumericCellValue() + "t");
                             break;
                         case Cell.CELL_TYPE_STRING:
-                            System.out.print(cell.getStringCellValue() + "t");
+                        	rowContents[i] = cell.getStringCellValue(); 
+                        	i++;
+                            //System.out.print(cell.getStringCellValue() + "t");
                             break;
                     }
                 }
+                my_data.add(new QuestionAnswer(rowContents[0],rowContents[1],rowContents[2],rowContents[3],editLevel));
                 System.out.println("");
             }
+            workbook.close();
             file.close();
+            QuestionAnswer remove = null;
+            for(QuestionAnswer cur : my_data)
+            {
+            	if(cur.toString().contains("KeyPhrase")){remove = cur;}
+            }
+            my_data.remove(remove);
+            for(QuestionAnswer cur : my_data)
+            {
+            	System.out.println(cur.toString());
+            }
+            
         }
         catch (Exception e)
         {
@@ -66,5 +91,6 @@ public class Database {
         }
 		
 	}
+	
 
 }
--- src/Model/QuestionAnswer.java
+++ src/Model/QuestionAnswer.java
@@ -10,16 +10,24 @@ package Model;
 public class QuestionAnswer {
 
 	private String my_Category;
-	private String [] my_KeyPhrases;
+	private String my_KeyPhrases;
 	private String my_SampleQuestion;
 	private String my_Answer;
 	private int my_EditLevel;
 	
-	public QuestionAnswer (String the_category, String [] the_key_phrases, String the_question, String the_answer, int the_level){		
+	public QuestionAnswer (String the_category, String the_key_phrases, String the_question, String the_answer, int the_level){		
 		my_Category = the_category;
-		my_KeyPhrases = the_key_phrases.clone();
+		my_KeyPhrases = the_key_phrases;
 		my_SampleQuestion = the_question;
 		my_Answer = the_answer;
 		my_EditLevel = the_level;
+	}
+	
+	@Override
+	public
+	String toString()
+	{
+		return my_Category + " " + my_KeyPhrases + " " + my_SampleQuestion + " " + my_Answer + " " + my_EditLevel;
+		
 	}
 }
