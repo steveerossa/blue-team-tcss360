834e0ce9a094d4ac8f9b43d3c2604af6684e2a57
jstowe77@uw.edu <jstowe77@uw.edu@385cc09a-a65a-aa53-844a-8a29cb244b3c>
2015-03-08 07:06:02 +0000

    Updated the build path and jar files included for the excel parsing. Also started working on populating the database.

---
+++ src/Model/DatabaseTest.java
@@ -0,0 +1,16 @@
+package Model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DatabaseTest {
+
+	@Test
+	public void testDatabase() {
+		
+		Database testing = new Database();
+		fail("Not yet implemented");
+	}
+
+}
