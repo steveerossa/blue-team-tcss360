2f1cc37e108e78e37c06753ef205ab98bac7f903
jstowe77@uw.edu <jstowe77@uw.edu@385cc09a-a65a-aa53-844a-8a29cb244b3c>
2015-03-08 07:06:02 +0000

    Updated the build path and jar files included for the excel parsing. Also started working on populating the database.

--- src/Model/Database.java
+++ src/Model/Database.java
@@ -2,6 +2,69 @@
 
 package Model;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class Database {
+	
+	List<QuestionAnswer> my_data;
+	
+	public Database()
+	{
+		my_data = new ArrayList<QuestionAnswer>();
+		populateData();
+	}
+
+	private void populateData() {
+		try
+        {
+            FileInputStream file = new FileInputStream(new File("src/files/Answers.xlsx"));
+            
+            //Create Workbook instance holding reference to .xlsx file
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+ 
+            //Get first/desired sheet from the workbook
+            XSSFSheet sheet = workbook.getSheetAt(0);
+ 
+            //Iterate through each rows one by one
+            Iterator<Row> rowIterator = sheet.iterator();
+            while (rowIterator.hasNext())
+            {
+                Row row = rowIterator.next();
+                //For each row, iterate through all the columns
+                Iterator<Cell> cellIterator = row.cellIterator();
+                 
+                while (cellIterator.hasNext())
+                {
+                    Cell cell = cellIterator.next();
+                    //Check the cell type and format accordingly
+                    switch (cell.getCellType())
+                    {
+                        case Cell.CELL_TYPE_NUMERIC:
+                            System.out.print(cell.getNumericCellValue() + "t");
+                            break;
+                        case Cell.CELL_TYPE_STRING:
+                            System.out.print(cell.getStringCellValue() + "t");
+                            break;
+                    }
+                }
+                System.out.println("");
+            }
+            file.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+		
+	}
 
 }
