5139c55751e212c35d8984ba66b861c9c093b1d7
aday17@uw.edu <aday17@uw.edu@385cc09a-a65a-aa53-844a-8a29cb244b3c>
2015-03-11 06:44:33 +0000

    formatting ftw

--- src/View/RFPView.java
+++ src/View/RFPView.java
@@ -1,6 +1,7 @@
 package View;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -16,7 +17,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -27,7 +27,6 @@ import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.KeyStroke;
-import javax.swing.ListModel;
 import javax.swing.ListSelectionModel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
@@ -73,7 +72,7 @@ public class RFPView
 
 		JMenuItem mntmSaveAs = new JMenuItem("Save As...");
 		mnFile.add(mntmSaveAs);
-		
+
 		JMenuItem mntmLogOut = new JMenuItem("Log out");
 		mntmLogOut.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -116,13 +115,14 @@ public class RFPView
 		GridBagLayout gbl_contentPane = new GridBagLayout();
 		gbl_contentPane.columnWidths = new int[]{127, 0, 226, 66, 200, 0};
 		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{1.0, 0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWeights = new double[]{1.5, 0.0, 1.2, 0.0, 1.5, Double.MIN_VALUE};
 		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
-		
+
 		final JTextArea answerTextArea = new JTextArea();
 		answerTextArea.setEditable(false);
 		answerTextArea.setLineWrap(true);
+		answerTextArea.setWrapStyleWord(true);
 		answerTextArea.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
 		GridBagConstraints gbc_textArea = new GridBagConstraints();
 		gbc_textArea.insets = new Insets(0, 0, 10, 5);
@@ -140,10 +140,7 @@ public class RFPView
 		gbc_textField.gridy = 0;
 		contentPane.add(searchTextField, gbc_textField);
 		searchTextField.setColumns(10);
-		
-		
-		
-		
+
 		final JList<QuestionAnswer> list = new JList<QuestionAnswer>(listModel);
 		list.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
 		list.setLayoutOrientation(JList.VERTICAL);
@@ -161,7 +158,8 @@ public class RFPView
 			@Override
 			public void valueChanged(ListSelectionEvent arg0) {
 				if(!list.isSelectionEmpty()){
-				answerTextArea.setText(list.getSelectedValue().getAnswer());
+					answerTextArea.setText("");
+					answerTextArea.append(list.getSelectedValue().getAnswer());
 				}
 			}			
 		});
@@ -181,7 +179,7 @@ public class RFPView
 				list.setListData(questionAnswerList);
 			}			
 		});
-		
+
 
 		final JScrollPane scrollPane = new JScrollPane();
 		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
@@ -217,7 +215,7 @@ public class RFPView
 		JButton btnAddQ = new JButton("");
 		btnAddQ.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 			}
 		});
 		btnAddQ.setIcon(new ImageIcon(RFPView.class.getResource("/files/addQuestionIcon.png")));
@@ -242,7 +240,7 @@ public class RFPView
 		contentPane.add(btnAddToClip, gbc_btnAddToClip);
 
 		my_mainFrame.repaint();
-		
+
 		searchTextField.addKeyListener(new KeyListener() {
 			@Override
 			public void keyPressed(KeyEvent e) {
@@ -255,44 +253,44 @@ public class RFPView
 				if(e.getKeyCode() == KeyEvent.VK_ENTER){
 					searchTextField.selectAll();
 				}
-				
+
 			}
 
 			@Override
 			public void keyTyped(KeyEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}			
 		});
-		
+
 	}
 
-	
-	
 
 
-//
-//		/**
-//		 * Launch the application.
-//		 */
-//		public static void main(String[] args) {
-//			EventQueue.invokeLater(new Runnable() {
-//				public void run() {
-//					try {
-//						UIManager.setLookAndFeel(
-//					            UIManager.getSystemLookAndFeelClassName());
-//						RFPView frame = new RFPView();
-//						frame.setVisible(true);
-//					} catch (Exception e) {
-//						e.printStackTrace();
-//					}
-//				}
-//			});
-//		}
-//
-//	/**
-//	 * Create the frame.
-//	 */
+
+
+	//
+	//		/**
+	//		 * Launch the application.
+	//		 */
+	//		public static void main(String[] args) {
+	//			EventQueue.invokeLater(new Runnable() {
+	//				public void run() {
+	//					try {
+	//						UIManager.setLookAndFeel(
+	//					            UIManager.getSystemLookAndFeelClassName());
+	//						RFPView frame = new RFPView();
+	//						frame.setVisible(true);
+	//					} catch (Exception e) {
+	//						e.printStackTrace();
+	//					}
+	//				}
+	//			});
+	//		}
+	//
+	//	/**
+	//	 * Create the frame.
+	//	 */
 
 
 	/**
@@ -300,172 +298,172 @@ public class RFPView
 	 */
 	public RFPView() {
 
-//				setTitle("Global Business Logistics");
-//				setIconImage(Toolkit.getDefaultToolkit().getImage(RFPView.class.getResource("/files/title_bar_icon.png")));
-//				setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//				setBounds(100, 100, 706, 468);
-//				setResizable(true);
-//				
-//				JMenuBar menuBar = new JMenuBar();
-//				setJMenuBar(menuBar);
-//				
-//				JMenu mnFile = new JMenu("File");
-//				menuBar.add(mnFile);
-//				
-//				JMenuItem mntmNew = new JMenuItem("New");
-//				mntmNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_MASK));
-//				mnFile.add(mntmNew);
-//				
-//				JMenuItem mntmOpen = new JMenuItem("Open...");
-//				mnFile.add(mntmOpen);
-//				
-//				JMenuItem mntmSave = new JMenuItem("Save");
-//				mntmSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
-//				mnFile.add(mntmSave);
-//				
-//				JMenuItem mntmSaveAs = new JMenuItem("Save As...");
-//				mnFile.add(mntmSaveAs);
-//				
-//				JMenuItem mntmExit = new JMenuItem("Exit");
-//				mntmExit.addActionListener(new ActionListener() {
-//					public void actionPerformed(ActionEvent arg0) {
-//						System.exit(0);
-//					}
-//				});
-//				
-//				JMenuItem mntmLogOut = new JMenuItem("Log out");
-//				mntmLogOut.addActionListener(new ActionListener() {
-//					public void actionPerformed(ActionEvent e) {
-//						LoginView loginView = new LoginView();
-//						loginView.initializeLogin(my_mainframe);
-//					}
-//				});
-//				mnFile.add(mntmLogOut);
-//				mnFile.add(mntmExit);
-//				
-//				JMenu mnEdit = new JMenu("Edit");
-//				menuBar.add(mnEdit);
-//				
-//				JMenu mnView = new JMenu("View");
-//				menuBar.add(mnView);
-//				
-//				JMenu mnHelp = new JMenu("Help");
-//				menuBar.add(mnHelp);
-//				
-//				JMenuItem mntmHelp = new JMenuItem("Help...");
-//				mnHelp.add(mntmHelp);
-//				
-//				JMenuItem mntmAbout = new JMenuItem("About");
-//				mnHelp.add(mntmAbout);
-//				contentPane = new JPanel();
-//				contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-//				setContentPane(contentPane);
-//				GridBagLayout gbl_contentPane = new GridBagLayout();
-//				gbl_contentPane.columnWidths = new int[]{127, 0, 226, 66, 200, 0};
-//				gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0};
-//				gbl_contentPane.columnWeights = new double[]{1.0, 0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
-//				gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
-//				contentPane.setLayout(gbl_contentPane);
-//				
-//				textField = new JTextField();
-//				GridBagConstraints gbc_textField = new GridBagConstraints();
-//				gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-//				gbc_textField.insets = new Insets(0, 0, 5, 5);
-//				gbc_textField.gridx = 0;
-//				gbc_textField.gridy = 0;
-//				contentPane.add(textField, gbc_textField);
-//				textField.setColumns(10);
-//				
-//				JButton btnSearch = new JButton("");
-//				btnSearch.addActionListener(new ActionListener() {
-//					public void actionPerformed(ActionEvent arg0) {
-//						
-//					}
-//				});
-//				btnSearch.setIcon(new ImageIcon(RFPView.class.getResource("/files/searchIcon.png")));
-//				GridBagConstraints gbc_btnSearch = new GridBagConstraints();
-//				gbc_btnSearch.insets = new Insets(0, 0, 5, 5);
-//				gbc_btnSearch.gridx = 1;
-//				gbc_btnSearch.gridy = 0;
-//				contentPane.add(btnSearch, gbc_btnSearch);
-//				
-//				JScrollPane scrollPane = new JScrollPane();
-//				GridBagConstraints gbc_scrollPane = new GridBagConstraints();
-//				gbc_scrollPane.fill = GridBagConstraints.BOTH;
-//				gbc_scrollPane.gridheight = 4;
-//				gbc_scrollPane.gridx = 4;
-//				gbc_scrollPane.gridy = 0;
-//				contentPane.add(scrollPane, gbc_scrollPane);
-//				
-//				JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-//				tabbedPane.setBorder(null);
-//				scrollPane.setViewportView(tabbedPane);
-//				tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
-//				
-//				JTextArea notesArea = new JTextArea();
-//				tabbedPane.addTab("Notes", null, notesArea, null);
-//				
-//				JList selectedQAsList = new JList();
-//				tabbedPane.addTab("Selected Q/A's", null, selectedQAsList, null);
-//				
-//				JComboBox comboBox = new JComboBox();
-//				GridBagConstraints gbc_comboBox = new GridBagConstraints();
-//				gbc_comboBox.gridwidth = 2;
-//				gbc_comboBox.insets = new Insets(0, 0, 5, 5);
-//				gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
-//				gbc_comboBox.gridx = 0;
-//				gbc_comboBox.gridy = 1;
-//				contentPane.add(comboBox, gbc_comboBox);
-//				
-//				JTextArea textArea = new JTextArea();
-//				textArea.setEditable(false);
-//				textArea.setLineWrap(true);
-//				textArea.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
-//				GridBagConstraints gbc_textArea = new GridBagConstraints();
-//				gbc_textArea.insets = new Insets(0, 0, 0, 5);
-//				gbc_textArea.gridheight = 3;
-//				gbc_textArea.fill = GridBagConstraints.BOTH;
-//				gbc_textArea.gridx = 2;
-//				gbc_textArea.gridy = 1;
-//				contentPane.add(textArea, gbc_textArea);
-//				
-//				JList list = new JList();
-//				list.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
-//				list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-//				GridBagConstraints gbc_list = new GridBagConstraints();
-//				gbc_list.gridwidth = 2;
-//				gbc_list.gridheight = 2;
-//				gbc_list.insets = new Insets(0, 0, 0, 5);
-//				gbc_list.fill = GridBagConstraints.BOTH;
-//				gbc_list.gridx = 0;
-//				gbc_list.gridy = 2;
-//				contentPane.add(list, gbc_list);
-//				
-//				JButton btnAddQ = new JButton("");
-//				btnAddQ.addActionListener(new ActionListener() {
-//					public void actionPerformed(ActionEvent e) {
-//					}
-//				});
-//				btnAddQ.setIcon(new ImageIcon(RFPView.class.getResource("/files/addQuestionIcon.png")));
-//				GridBagConstraints gbc_btnAddQ = new GridBagConstraints();
-//				gbc_btnAddQ.anchor = GridBagConstraints.SOUTH;
-//				gbc_btnAddQ.insets = new Insets(0, 0, 5, 5);
-//				gbc_btnAddQ.gridx = 3;
-//				gbc_btnAddQ.gridy = 2;
-//				contentPane.add(btnAddQ, gbc_btnAddQ);
-//				
-//				JButton btnAddToClip = new JButton("");
-//				btnAddToClip.addActionListener(new ActionListener() {
-//					public void actionPerformed(ActionEvent e) {
-//					}
-//				});
-//				btnAddToClip.setIcon(new ImageIcon(RFPView.class.getResource("/files/copyToClipIcon.png")));
-//				GridBagConstraints gbc_btnAddToClip = new GridBagConstraints();
-//				gbc_btnAddToClip.anchor = GridBagConstraints.NORTH;
-//				gbc_btnAddToClip.insets = new Insets(0, 0, 0, 5);
-//				gbc_btnAddToClip.gridx = 3;
-//				gbc_btnAddToClip.gridy = 3;
-//				contentPane.add(btnAddToClip, gbc_btnAddToClip);
+		//				setTitle("Global Business Logistics");
+		//				setIconImage(Toolkit.getDefaultToolkit().getImage(RFPView.class.getResource("/files/title_bar_icon.png")));
+		//				setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//				setBounds(100, 100, 706, 468);
+		//				setResizable(true);
+		//				
+		//				JMenuBar menuBar = new JMenuBar();
+		//				setJMenuBar(menuBar);
+		//				
+		//				JMenu mnFile = new JMenu("File");
+		//				menuBar.add(mnFile);
+		//				
+		//				JMenuItem mntmNew = new JMenuItem("New");
+		//				mntmNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_MASK));
+		//				mnFile.add(mntmNew);
+		//				
+		//				JMenuItem mntmOpen = new JMenuItem("Open...");
+		//				mnFile.add(mntmOpen);
+		//				
+		//				JMenuItem mntmSave = new JMenuItem("Save");
+		//				mntmSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
+		//				mnFile.add(mntmSave);
+		//				
+		//				JMenuItem mntmSaveAs = new JMenuItem("Save As...");
+		//				mnFile.add(mntmSaveAs);
+		//				
+		//				JMenuItem mntmExit = new JMenuItem("Exit");
+		//				mntmExit.addActionListener(new ActionListener() {
+		//					public void actionPerformed(ActionEvent arg0) {
+		//						System.exit(0);
+		//					}
+		//				});
+		//				
+		//				JMenuItem mntmLogOut = new JMenuItem("Log out");
+		//				mntmLogOut.addActionListener(new ActionListener() {
+		//					public void actionPerformed(ActionEvent e) {
+		//						LoginView loginView = new LoginView();
+		//						loginView.initializeLogin(my_mainframe);
+		//					}
+		//				});
+		//				mnFile.add(mntmLogOut);
+		//				mnFile.add(mntmExit);
+		//				
+		//				JMenu mnEdit = new JMenu("Edit");
+		//				menuBar.add(mnEdit);
+		//				
+		//				JMenu mnView = new JMenu("View");
+		//				menuBar.add(mnView);
+		//				
+		//				JMenu mnHelp = new JMenu("Help");
+		//				menuBar.add(mnHelp);
+		//				
+		//				JMenuItem mntmHelp = new JMenuItem("Help...");
+		//				mnHelp.add(mntmHelp);
+		//				
+		//				JMenuItem mntmAbout = new JMenuItem("About");
+		//				mnHelp.add(mntmAbout);
+		//				contentPane = new JPanel();
+		//				contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		//				setContentPane(contentPane);
+		//				GridBagLayout gbl_contentPane = new GridBagLayout();
+		//				gbl_contentPane.columnWidths = new int[]{127, 0, 226, 66, 200, 0};
+		//				gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0};
+		//				gbl_contentPane.columnWeights = new double[]{1.0, 0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
+		//				gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
+		//				contentPane.setLayout(gbl_contentPane);
+		//				
+		//				textField = new JTextField();
+		//				GridBagConstraints gbc_textField = new GridBagConstraints();
+		//				gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		//				gbc_textField.insets = new Insets(0, 0, 5, 5);
+		//				gbc_textField.gridx = 0;
+		//				gbc_textField.gridy = 0;
+		//				contentPane.add(textField, gbc_textField);
+		//				textField.setColumns(10);
+		//				
+		//				JButton btnSearch = new JButton("");
+		//				btnSearch.addActionListener(new ActionListener() {
+		//					public void actionPerformed(ActionEvent arg0) {
+		//						
+		//					}
+		//				});
+		//				btnSearch.setIcon(new ImageIcon(RFPView.class.getResource("/files/searchIcon.png")));
+		//				GridBagConstraints gbc_btnSearch = new GridBagConstraints();
+		//				gbc_btnSearch.insets = new Insets(0, 0, 5, 5);
+		//				gbc_btnSearch.gridx = 1;
+		//				gbc_btnSearch.gridy = 0;
+		//				contentPane.add(btnSearch, gbc_btnSearch);
+		//				
+		//				JScrollPane scrollPane = new JScrollPane();
+		//				GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		//				gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		//				gbc_scrollPane.gridheight = 4;
+		//				gbc_scrollPane.gridx = 4;
+		//				gbc_scrollPane.gridy = 0;
+		//				contentPane.add(scrollPane, gbc_scrollPane);
+		//				
+		//				JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		//				tabbedPane.setBorder(null);
+		//				scrollPane.setViewportView(tabbedPane);
+		//				tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+		//				
+		//				JTextArea notesArea = new JTextArea();
+		//				tabbedPane.addTab("Notes", null, notesArea, null);
+		//				
+		//				JList selectedQAsList = new JList();
+		//				tabbedPane.addTab("Selected Q/A's", null, selectedQAsList, null);
+		//				
+		//				JComboBox comboBox = new JComboBox();
+		//				GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		//				gbc_comboBox.gridwidth = 2;
+		//				gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+		//				gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+		//				gbc_comboBox.gridx = 0;
+		//				gbc_comboBox.gridy = 1;
+		//				contentPane.add(comboBox, gbc_comboBox);
+		//				
+		//				JTextArea textArea = new JTextArea();
+		//				textArea.setEditable(false);
+		//				textArea.setLineWrap(true);
+		//				textArea.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		//				GridBagConstraints gbc_textArea = new GridBagConstraints();
+		//				gbc_textArea.insets = new Insets(0, 0, 0, 5);
+		//				gbc_textArea.gridheight = 3;
+		//				gbc_textArea.fill = GridBagConstraints.BOTH;
+		//				gbc_textArea.gridx = 2;
+		//				gbc_textArea.gridy = 1;
+		//				contentPane.add(textArea, gbc_textArea);
+		//				
+		//				JList list = new JList();
+		//				list.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		//				list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		//				GridBagConstraints gbc_list = new GridBagConstraints();
+		//				gbc_list.gridwidth = 2;
+		//				gbc_list.gridheight = 2;
+		//				gbc_list.insets = new Insets(0, 0, 0, 5);
+		//				gbc_list.fill = GridBagConstraints.BOTH;
+		//				gbc_list.gridx = 0;
+		//				gbc_list.gridy = 2;
+		//				contentPane.add(list, gbc_list);
+		//				
+		//				JButton btnAddQ = new JButton("");
+		//				btnAddQ.addActionListener(new ActionListener() {
+		//					public void actionPerformed(ActionEvent e) {
+		//					}
+		//				});
+		//				btnAddQ.setIcon(new ImageIcon(RFPView.class.getResource("/files/addQuestionIcon.png")));
+		//				GridBagConstraints gbc_btnAddQ = new GridBagConstraints();
+		//				gbc_btnAddQ.anchor = GridBagConstraints.SOUTH;
+		//				gbc_btnAddQ.insets = new Insets(0, 0, 5, 5);
+		//				gbc_btnAddQ.gridx = 3;
+		//				gbc_btnAddQ.gridy = 2;
+		//				contentPane.add(btnAddQ, gbc_btnAddQ);
+		//				
+		//				JButton btnAddToClip = new JButton("");
+		//				btnAddToClip.addActionListener(new ActionListener() {
+		//					public void actionPerformed(ActionEvent e) {
+		//					}
+		//				});
+		//				btnAddToClip.setIcon(new ImageIcon(RFPView.class.getResource("/files/copyToClipIcon.png")));
+		//				GridBagConstraints gbc_btnAddToClip = new GridBagConstraints();
+		//				gbc_btnAddToClip.anchor = GridBagConstraints.NORTH;
+		//				gbc_btnAddToClip.insets = new Insets(0, 0, 0, 5);
+		//				gbc_btnAddToClip.gridx = 3;
+		//				gbc_btnAddToClip.gridy = 3;
+		//				contentPane.add(btnAddToClip, gbc_btnAddToClip);
 	}
 
 }
