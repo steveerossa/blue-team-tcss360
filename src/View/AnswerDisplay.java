b8776038880aa3be07cd3a261958a9f87cade6f2
soo2@uw.edu <soo2@uw.edu@385cc09a-a65a-aa53-844a-8a29cb244b3c>
2015-03-11 00:43:52 +0000

    Steve AnswerDisplay

--- src/View/AnswerDisplay.java
+++ src/View/AnswerDisplay.java
@@ -1,3 +1,4 @@
+
 //steve
 
 package View;
@@ -6,10 +7,14 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+//import javax.swing.JToggleButton;
 
 /**Class AnswerDisplay is a JPanel that displays the questions and answers.*/
 public class AnswerDisplay extends JPanel 
@@ -20,33 +25,44 @@ public class AnswerDisplay extends JPanel
 	/**JCombobox that has all the keys for searching database.*/
 	private JComboBox<String> my_key_box ;
 	
+	/**JTextField to enter search key.*/
+	private JTextField my_key_field;
+
+	/**JList which contains all the pre-selection questions. */
+	private JList<String> my_question_list;
+	
 	/**Textfield that displays the answers to be pasted into the RFP.*/
-	private JTextArea my_answer_field;
+	private JTextArea my_answer_area;
 	
 	/**The pre-selected questions.*/
 //	private String [] my_questions;
 	
-	/**Array of strings that contains keys for querying the database.*/
-	private String [] my_keys;
+	/**Array of strings that contains keys for querying the database. This will
+	 * be used in the JComboBox.*/
+	private String [] my_keys = {"ONE","TWO","THREE"} ; // one two tree for testing
 	
-	/**JList which contains all the pre-selection questions. */
-	private JList<String> my_question_list;
-	/**JTextField to enter search key.*/
-	private JTextField my_field;
+	
+	
 	
 	/**The Constructor.*/
 	public AnswerDisplay ()
 	{
-		my_answer_field  = new JTextArea();
-		my_field  = new JTextField();
-		my_question_list = new JList<>();
+		setLayout(new BorderLayout()); // borderLayout
+		
+		my_answer_area  = new JTextArea();
+		my_key_field  = new JTextField();
+		my_question_list = new JList<>(my_keys);
+		
 		
 		
 		my_key_box = new JComboBox<String>(my_keys);
 		my_key_box.addItem("String");
 		my_key_box.addActionListener(my_key_box);
-		this.add(questionPanel(), BorderLayout.WEST);
+		this.add(queryPanel(), BorderLayout.WEST);
 		this.add(answerPanel(), BorderLayout.EAST);
+		this.setVisible(true);
+		//add(answerPanel());
+		//add("ssssssssssss" ,new JToggleButton("This Button!"));
 		
 		
 	} // end constructor 
@@ -54,32 +70,33 @@ public class AnswerDisplay extends JPanel
 	/**JPanel that displays the answers.*/
 	private JPanel answerPanel()
 	{
-		JPanel panel  = new JPanel();
-		panel.add("Answer to Questions", my_answer_field);
+		JPanel panel  = new JPanel(new BorderLayout());
+		
+		panel.add(new JLabel("Answers to Questions"), BorderLayout.NORTH);
+		panel.add(my_answer_area, BorderLayout.CENTER);
 		
 		return panel;
 		
 	}
-	/**JPanel that displays the Key and Questions.*/
-	private JPanel questionPanel()
+	/**JPanel that displays the Keys and Questions.*/
+	private JPanel queryPanel()
 	{
 		
-		JPanel panel  = new JPanel();
-		panel.setLayout(new BorderLayout());
+		JPanel panel  = new JPanel(new BorderLayout());
 		
-		JPanel northPanel  = new JPanel(new GridLayout(1,2));
-		northPanel.add("Select Keys form Menu", my_key_box);
-		northPanel.add("Type Key to Search", my_field);
+		JPanel northPanel  = new JPanel(new GridLayout(2,2));
+		northPanel.add( new JLabel("Select key"));
+		northPanel.add( new JLabel("Type key to search"));
+		northPanel.add(new JScrollPane( my_key_box));
+		northPanel.add(my_key_field);
 		panel.add(northPanel, BorderLayout.NORTH);
 		
-		JPanel southPanel  = new JPanel();
-		southPanel.add("List of Questions.\n " +
-				"Click on Question in List to reveal answer.", my_question_list);
-		panel.add(southPanel, BorderLayout.NORTH);
-		
-		
-		
-		
+		JPanel centerPanel  = new JPanel(new BorderLayout());
+		centerPanel.add(new JLabel("List of questions"), BorderLayout.NORTH);
+		centerPanel.add(my_question_list, BorderLayout.CENTER);
+		panel.add(centerPanel, BorderLayout.CENTER);
+		repaint();
+	
 		return panel;
 		
 	}
@@ -87,8 +104,16 @@ public class AnswerDisplay extends JPanel
 	/**Testing class*/
 	public static void main(String... arguments)
 	{
+
 		System.out.println("Working");
-	}// end main
+		//new AnswerDisplay();
+		JFrame frame  = new JFrame("Frame Here");
+		frame.setVisible(true);
+		frame.setSize(400, 400);
+		frame.add(new AnswerDisplay());
+		frame.setVisible(true);
+
+	}
 	
 	
 	
@@ -96,4 +121,3 @@ public class AnswerDisplay extends JPanel
 	 
 
 } // end class
-
